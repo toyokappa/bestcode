184542d564c6ac7fdc8a71b96c54e351aa64427b
Yuta Toyokawa <kppg42@gmail.com>
2018-05-14 19:02:31 +0900

    rubocop -a --only Layout/SpaceInsideBlockBraces

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require "factory_bot_rails"
 require "shoulda-matchers"
 require "database_cleaner"
 
-Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].each {|f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
