a2fdf5c7d252caabe81da17abac6df28f0471632
Yuta Toyokawa <kppg42@gmail.com>
2018-05-16 01:38:28 +0900

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def callback
+    auth = request.env["omniauth.auth"]
+    user = User.find_by(provider: auth.provider, uid: auth.uid) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+  
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/auth/:provider/callback", to: "sessions#callback"
+  delete "/signout" => "sessions#destroy"
 end
