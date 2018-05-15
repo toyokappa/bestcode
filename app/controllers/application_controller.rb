fcf03f8226697789909561be699c4aefb8fdd00d
Yuta Toyokawa <kppg42@gmail.com>
2018-05-16 01:48:14 +0900

    Add authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :user_signed_in?
+  before_action :authenticate_user
 
   private
     
+    def authenticate_user
+      redirect_to root_path, alert: t(:authentication_alert, scope: :flash) unless user_signed_in?
+    end
+
     def current_user
       @current_user ||= User.find_by(id: session[:user_id])
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate_user, only: [:callback]
+  
   def callback
     auth = request.env["omniauth.auth"]
     user = User.find_by(provider: auth.provider, uid: auth.uid) || User.create_with_omniauth(auth)
