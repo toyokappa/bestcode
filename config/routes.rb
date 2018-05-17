bbf2a2739453366726f86c35f95f6695c444b54b
Yuta Toyokawa <kppg42@gmail.com>
2018-05-18 00:15:01 +0900

    Update welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
   get "/auth/:provider/callback", to: "sessions#callback"
-  delete "/signout", to: "sessions#destroy"
+  delete "/sign_out", to: "sessions#destroy"
 
   resources :rooms
   resources :participations, only: [:update, :destroy]
