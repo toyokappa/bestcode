1b1102dbeed00ce45debb65406d87c8c1fcdb46d
Yuta Toyokawa <kppg42@gmail.com>
2018-05-18 12:51:48 +0900

    Skip rubocop Metrics/LineLength and Metrics/BlockLength

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,52 +1,17 @@
-# frozen_string_literal: true
-
-# Please do not make direct changes to this file!
-# This generator is maintained by the community around simple_form-bootstrap:
-# https://github.com/rafaelfranca/simple_form-bootstrap
-# All future development, tests, and organization should happen there.
-# Background history: https://github.com/plataformatec/simple_form/issues/1561
-
-# Uncomment this and change the path if necessary to include your own
-# components.
-# See https://github.com/plataformatec/simple_form#custom-components
-# to know more about custom components.
-# Dir[Rails.root.join('lib/components/**/*.rb')].each { |f| require f }
-
-# Use this setup block to configure all options available in SimpleForm.
+# NOTE: rubocopのMetrics/LineLength, Metrics/BlockLengthに引っかかるので除外
+# rubocop:disable Metrics/LineLength, Metrics/BlockLength
 SimpleForm.setup do |config|
-  # Default class for buttons
   config.button_class = "btn"
-
-  # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = "form-check-label"
-
-  # How the label text should be generated altogether with the required text.
   config.label_text = ->(label, required, _explicit_label) { "#{label} #{required}" }
-
-  # Define the way to render check boxes / radio buttons with labels.
   config.boolean_style = :inline
-
-  # You can wrap each item in a collection of radio/check boxes with a tag
   config.item_wrapper_tag = :div
-
-  # Defines if the default input wrapper class should be included in radio
-  # collection wrappers.
   config.include_default_input_wrapper_class = false
-
-  # CSS class to add for error notification helper.
   config.error_notification_class = "alert alert-danger"
-
-  # Method used to tidy up errors. Specify any Rails Array method.
-  # :first lists the first message for each field.
-  # :to_sentence to list all errors for each field.
   config.error_method = :to_sentence
-
-  # add validation classes to `input_field`
   config.input_field_error_class = "is-invalid"
   config.input_field_valid_class = "is-valid"
 
-  # vertical forms
-  #
   # vertical default_wrapper
   config.wrappers :vertical_form, tag: "div", class: "form-group", error_class: "form-group-invalid", valid_class: "form-group-valid" do |b|
     b.use :html5
@@ -430,3 +395,4 @@ SimpleForm.setup do |config|
   #   time:          :custom_multi_select
   # }
 end
+# rubocop:enable Metrics/LineLength, Metrics/BlockLength
