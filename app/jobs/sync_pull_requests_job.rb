9c4e316745fdd86f8f0fd3e82483771b9e939857
Yuta Toyokawa <kppg42@gmail.com>
2018-05-21 12:19:52 +0900

    Create sync pull requests job

---
+++ app/jobs/sync_pull_requests_job.rb
@@ -0,0 +1,17 @@
+class SyncPullRequestsJob < ApplicationJob
+  queue_as :default
+
+  def perform(user, repo)
+    github_user = Octokit::Client.new(access_token: user.access_token, per_page: 100)
+    github_user.login
+
+    github_user.pulls(repo.name, state: "all").each do |github_pull|
+      next if github_pull.blank?
+      if pull = repo.pull_requests.find_by(id: github_pull.id)
+        pull.sync!(github_pull)
+      else
+        repo.create_pull_request!(github_pull)
+      end
+    end
+  end
+end
