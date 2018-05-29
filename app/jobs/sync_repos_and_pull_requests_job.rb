b0f0aa784a3330411ac9d186a07a51e40673ee7a
Yuta Toyokawa <kppg42@gmail.com>
2018-05-30 00:13:26 +0900

    repositoryをrepoへ変更: Jobまで

--- app/jobs/sync_repos_and_pull_requests_job.rb
+++ app/jobs/sync_repos_and_pull_requests_job.rb
@@ -1,15 +1,15 @@
-class SyncRepositoriesAndPullRequestsJob < ApplicationJob
+class SyncReposAndPullRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(user)
     github_user = Octokit::Client.new(access_token: user.access_token, per_page: 100)
     github_user.login
     github_user.repos({}, query: { type: "owner" }).each do |github_repo|
-      repo = user.repositories.find_by(id: github_repo.id)
+      repo = user.repos.find_by(id: github_repo.id)
       if repo
         repo.sync!(github_repo)
       else
-        repo = user.create_repository!(github_repo)
+        repo = user.create_repo!(github_repo)
       end
 
       github_user.pulls(repo.id, state: "all").each do |github_pull|
