2093340e0be5830c6d958e927d28d444666ba92c
Yuta Toyokawa <kppg42@gmail.com>
2018-06-04 19:36:59 +0900

    リポジトリ閲覧権限の変更: ControllerとRouting

--- app/controllers/users/pulls_controller.rb
+++ app/controllers/users/pulls_controller.rb
@@ -1,14 +1,13 @@
 class Users::PullsController < ApplicationController
   def show
-    @user = User.find_by!(name: params[:user_name])
-    @repo = @user.repos.find_by!(name: params[:repo_name])
-    @pull = @repo.pulls.find(params[:pull_id])
+    @repo = current_user.repos.find(params[:repo_id])
+    @pull = @repo.pulls.find(params[:id])
   end
 
   def update
     repo = current_user.repos.find(params[:repo_id])
     SyncPullsJob.perform_later(current_user, repo)
     flash[:success] = "GitHubとの同期を開始しました。少々時間をおいてからリロードしてください"
-    redirect_to users_repo_path(current_user.name, repo.name)
+    redirect_to users_repo_path(repo)
   end
 end
