bac4b4bd4474a185f5101ec15b55f869d8ebe2c4
Yuta Toyokawa <kppg42@gmail.com>
2018-06-18 18:07:23 +0900

    レビューリクエストの実装: レビューステータスでコメントを制限

--- app/models/review_comment.rb
+++ app/models/review_comment.rb
@@ -8,4 +8,30 @@ class ReviewComment < ApplicationRecord
 
   validates :body, presence: true, if: :commented?
   validates :body, length: { maximum: 10000 }
+  validates :state, presence: true
+
+  class << self
+    def selectable_state(user, room, review_req)
+      if user == room.reviewer
+        if review_req.state == :wait_review
+          [:commented, :change_request, :approved]
+        else
+          [:commented]
+        end
+      else
+        return [:commented, :reopen] if !review_req.is_open && !review_req.resolved?
+
+        case review_req.state.to_sym
+        when :change_request
+          [:commented, :rereview_request, :closed]
+        when :approved
+          [:commented, :resolved]
+        when :resolved
+          [:commented]
+        else
+          [:commented, :closed]
+        end
+      end
+    end
+  end
 end
