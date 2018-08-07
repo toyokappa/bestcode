10dc6c82d1d120a099288fa4bf1699d4d5dec119
Yuta Toyokawa <kppg42@gmail.com>
2018-08-08 00:38:56 +0900

    初回ログイン時のモーダル表示のJSを実装

--- frontend/javascripts/application.js
+++ frontend/javascripts/application.js
@@ -5,8 +5,10 @@ import * as ActiveStorage from "activestorage";
 import '../stylesheets/application.sass';
 
 import EditComment from './users/rooms/review_comments/edit_comment';
+import InitModal from './users/top/init_modal';
 
 document.addEventListener("DOMContentLoaded", () => {
   if($(".select2")[0]) { $(".select2").select2(); }
   if($(".ajax-comment-field")[0]) { new EditComment($(".ajax-comment-field")); }
+  if($(".first-time-user")[0]) { new InitModal($(".first-time-user")); }
 });
