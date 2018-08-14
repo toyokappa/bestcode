0a2471ab325b4f9013c7cfc6ffc903f70280a5c6
Yuta Toyokawa <kppg42@gmail.com>
2018-08-14 23:18:42 +0900

    ログイン中かどうかの確認

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user
+  before_action :check_sign_in
   layout "authentication"
 
   def index
   end
+
+  private
+
+    def check_sign_in
+      redirect_to users_root_path if user_signed_in?
+    end
 end
