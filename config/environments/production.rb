e08a90a5a4a90a10af75ddee5b80364643ad4363
Yuta Toyokawa <kppg42@gmail.com>
2018-08-20 21:59:28 +0900

    webpack_asset_pathの設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,8 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/bestcode-app'
+  config.assets.prefix = '/production/assets'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
