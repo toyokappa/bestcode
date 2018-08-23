bb0200ba7d5144a91e4bebadfb49e1860d39bf47
Yuta Toyokawa <kppg42@gmail.com>
2018-08-23 23:27:12 +0900

    sidekiqでメールが飛ばないバグの修正

--- bin/start_dev_sidekiq.sh
+++ bin/start_dev_sidekiq.sh
@@ -5,4 +5,4 @@ export DISABLE_SPRING=1
 
 echo Start bundle install
 bundle install --path=vendor/bundle --clean
-RAILS_ENV=${RAILS_ENV} bundle exec sidekiq
+RAILS_ENV=${RAILS_ENV} bundle exec sidekiq -q default -q mailers
--- bin/start_sidekiq.sh
+++ bin/start_sidekiq.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-RAILS_ENV=${RAILS_ENV} bundle exec sidekiq
+RAILS_ENV=${RAILS_ENV} bundle exec sidekiq -q default -q mailers
