ce9c5e20c1c86fc2387c9486eb606343ddaad203
Yuta Toyokawa <kppg42@gmail.com>
2018-08-31 23:55:11 +0900

    チャットの実装: チャットルームのデザイン微修正

--- app/controllers/users/chats_controller.rb
+++ app/controllers/users/chats_controller.rb
@@ -6,22 +6,23 @@ class Users::ChatsController < ApplicationController
     @title = "チャット"
     return if params[:reviewee_id].blank?
 
-    reviewee = @room.reviewees.find_by(id: params[:reviewee_id])
-    return flash.now[:danger] = "不正なチャットルームです" unless reviewee
+    @reviewee = @room.reviewees.find_by(id: params[:reviewee_id])
+    @reviewer = @room.reviewer
+    return flash.now[:danger] = "不正なチャットルームです" unless @reviewee
 
     gon.auth_token = current_user.firebase_auth_token
     gon.room_chat_id = @room.chat_id(params[:reviewee_id])
     gon.current_user = { id: current_user.chat_id }
     gon.users_info = {
       reviewer: {
-        id: @room.reviewer.chat_id,
-        image: @room.reviewer.image.url,
-        profile_path: users_profile_path(@room.reviewer.name)
+        id: @reviewer.chat_id,
+        image: @reviewer.image.url,
+        profile_path: users_profile_path(@reviewer.name)
       },
       reviewee: {
-        id: reviewee.chat_id,
-        image: reviewee.image.url,
-        profile_path: users_profile_path(reviewee.name)
+        id: @reviewee.chat_id,
+        image: @reviewee.image.url,
+        profile_path: users_profile_path(@reviewee.name)
       }
     }
   end
