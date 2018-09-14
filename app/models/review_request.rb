fe66316579b4c66b1215f8570352cb9d06e56f70
Yuta Toyokawa <kppg42@gmail.com>
2018-09-14 20:20:39 +0900

    リファクタリング: 不要なバリデーションの削除

--- app/models/review_request.rb
+++ app/models/review_request.rb
@@ -21,13 +21,6 @@ class ReviewRequest < ApplicationRecord
   belongs_to :reviewee, class_name: "User", inverse_of: :review_requests
   belongs_to :room, inverse_of: :review_assigns
 
-  validates :name, presence: true, length: { maximum: 1000 }
-  validates :is_open, inclusion: { in: [true, false] }
-  validates :state, presence: true
-  validates :pull_id, presence: true
-  validates :reviewee_id, presence: true
-  validates :room_id, presence: true
-
   enumerize :state, in: [:wait_review, :commented, :changes_requested, :approved, :resolved], predicates: true
 
   def state_color
