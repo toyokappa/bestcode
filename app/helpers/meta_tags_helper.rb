c275728a841b7f3e5c2ae4f00aae3996bda473c8
Yuta Toyokawa <kppg42@gmail.com>
2018-10-17 08:00:24 +0900

    Rubocop指摘事項の修正

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,14 +1,14 @@
 module MetaTagsHelper
   def default_meta_tags(title = nil)
     {
-      site: title.present? ? "#{title} | #{t('app.title')}" : t("app.title"),
+      site: title.present? ? "#{title} | #{t("app.title")}" : t("app.title"),
       reverse: true,
       charset: "utf-8",
       description: t("app.description"),
       keyword: t("app.keyword"),
       og: {
         site_name: t("app.title"),
-        title: title.present? ? "#{title} | #{t('app.title')}" : t("app.title"),
+        title: title.present? ? "#{title} | #{t("app.title")}" : t("app.title"),
         description: t("app.description"),
         type: "website",
         url: request.original_url,
@@ -17,7 +17,7 @@ module MetaTagsHelper
       },
       twitter: {
         card: "summary large image",
-        title: title.present? ? "#{title} | #{t('app.title')}" : t("app.title"),
+        title: title.present? ? "#{title} | #{t("app.title")}" : t("app.title"),
         description: t("app.description"),
         site: "@toyokappa",
       },
