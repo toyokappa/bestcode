7c7cfe336daaf5b931b1f850702c7d76be995707
Yuta Toyokawa <kppg42@gmail.com>
2018-10-20 22:52:52 +0900

    emailが読み込めるOauth権限へ変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  scope = "read:user, user:email, repo:status, admin:repo_hook"
+  scope = "user, repo:status, admin:repo_hook"
   provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: scope
 end
